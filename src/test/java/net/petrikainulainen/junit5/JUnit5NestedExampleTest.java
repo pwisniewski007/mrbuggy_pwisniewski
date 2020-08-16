cd9c15b63d3297450a04da45cf8ebaae5734cc9a
Joanna Stachiewicz <joanna.stachiewicz@intive.com>
2020-08-16 20:29:42 +0200

    added tests

--- src/test/java/net/petrikainulainen/junit5/JUnit5NestedExampleTest.java
+++ src/test/java/net/petrikainulainen/junit5/JUnit5NestedExampleTest.java
@@ -1,37 +1,86 @@
 package net.petrikainulainen.junit5;
 
-import static io.restassured.RestAssured.delete;
+import static io.restassured.RestAssured.*;
 import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
+
 import static org.hamcrest.Matchers.hasItems;
+
 import org.junit.jupiter.api.*;
 import org.hamcrest.Matchers;
 
-
+import org.json.JSONObject;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class JUnit5NestedExampleTest {
 
+	private static final String URL_GET = "http://localhost:8080";
 
 
 	@Test
-	public void simple_get_test() {
-		
-   given().queryParam("CUSTOMER_ID","68195")
-           .queryParam("PASSWORD","1234!")
-           .queryParam("Account_No","1")
-           .when().get("http://demo.guru99.com/V4/sinkministatement.php").then().log()
-           .body();
-	}
-	@Test
-	public void test2()
+	public void testGetUsers()
 	{
-		int statusCode= given().queryParam("CUSTOMER_ID","68195")
-           .queryParam("PASSWORD","1234!")
-           .queryParam("Account_No","1") .when().get("http://demo.guru99.com/V4/sinkministatement.php").getStatusCode();
-   System.out.println("The response status is "+statusCode);
+		String result = given().headers(
+				"Authorization", "cHJ6ZW1lazI=:UHJ6ZW1lazEyMyE=",
+				"Accept", "application/json",
+				"Content-Type","application/x-www-form-urlencoded")
+				.when()
+				.get(URL_GET + "/user")
+				.then()
+				.statusCode(200)
+				.extract()
+				.asString();
+		System.out.println(result);
+	}
 
-   given().when().get(url).then().assertThat().statusCode(200);
+	@Test
+	public void testPostAdmin()
+	{
+
+		String response = given()
+				//				.urlEncodingEnabled(true)
+				.param("username", "przemek32")
+				.param("first_name", "Testowy")
+				.param("last_name", "User")
+				.param("email", "test321218@test.com")
+				.param("phone_number", "+48981232853")
+				.param("password", "tajnehaslo9$")
+				.headers(
+						"Authorization", "cHJ6ZW1lazI=:UHJ6ZW1lazEyMyE=",
+						"Accept", "application/json",
+						"Content-Type", "application/x-www-form-urlencoded")
+				.when()
+				.post(URL_GET + "/user/admin")
+				.then().statusCode(200)
+				.extract()
+				.asString();
+		System.out.println(response);
+	}
+
+	@Test
+	public void editUser()
+	{
+
+		String response = given()
+				.contentType(ContentType.URLENC)
+				.formParam("username", "przemek323")
+				.formParam("first_name", "dupsko")
+				.formParam("last_name", "UserZmieniony")
+				.formParam("email", "dupsko@o2.pl")
+				.formParam("phone_number", "333444555")
+				.formParam("password", "password123!")
+				.headers(
+						"Authorization", "cHJ6ZW1lazI=:UHJ6ZW1lazEyMyE=",
+						"Accept", "application/json",
+						"Content-Type", "application/x-www-form-urlencoded;charset=UTF-8"
+				)
+				.when()
+				.put( URL_GET + "/user/5")
+				.then()
+				.extract()
+				.asString();
+
+		System.out.println(URL_GET + "/user/5");
+		System.out.println(response);
 	}
 }
